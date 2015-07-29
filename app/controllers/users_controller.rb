237343c759028bd51fe889c4c02618da72e68e7e
bagdenia <bagdenia@gmail.com>
2015-07-30 00:39:52 +0300

    Users controller and view added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,36 @@
 class UsersController < ApplicationController
- #before_action :set_user, only: [:show, :update]
-
-  # GET /pets
-  # GET /pets.json
-  def index
-    @users = User.includes(pets: :images).all
+  before_action :set_user, only: [:show]
+  def set_user
+    @user = User.find(params[:id])
   end
 
-  # GET /pets/1
-  # GET /pets/1.json
+  before_action :set_current_user, only: [:update, :edit]
+  def set_current_user
+    @user = current_user
+  end
+
+  def index
+    @users = User.includes(pets: :images).order(:email)
+  end
+
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to @user, notice: 'user was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:email)
+    end
 end
